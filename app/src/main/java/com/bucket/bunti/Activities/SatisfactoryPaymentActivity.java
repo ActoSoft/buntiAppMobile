68bcfab0b2d797ff80611b56eb3d37fd88bd03c6
Martin Melo <nitram-210397@hotmail.com>
2019-08-04 17:33:08 -0500

    Push notification are working

--- app/src/main/java/com/bucket/bunti/Activities/SatisfactoryPaymentActivity.java
+++ app/src/main/java/com/bucket/bunti/Activities/SatisfactoryPaymentActivity.java
@@ -2,15 +2,23 @@ package com.bucket.bunti.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.bucket.bunti.R;
 
 public class SatisfactoryPaymentActivity extends AppCompatActivity {
 
+    private TextView textViewMinutes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_satisfactory_payment);
+        Intent intent = getIntent();
+        String minutes = intent.getStringExtra("minutes");
+        textViewMinutes = findViewById(R.id.textViewMinutes);
+        textViewMinutes.setText(String.format("%s", minutes));
     }
 }
--- app/src/main/java/com/bucket/bunti/Service/MyFirebaseInstanceService.java
+++ app/src/main/java/com/bucket/bunti/Service/MyFirebaseInstanceService.java
@@ -3,20 +3,22 @@ package com.bucket.bunti.Service;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.util.Log;
-
+import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
+import com.bucket.bunti.Activities.SatisfactoryPaymentActivity;
 import com.bucket.bunti.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -43,6 +45,11 @@ public class MyFirebaseInstanceService extends FirebaseMessagingService {
 
         String title = data.get("title").toString();
         String body = data.get("body").toString();
+        String minutes = "";
+        if(data.get("minutes") != null) {
+             minutes = data.get("minutes").toString();
+        }
+
 
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         String NOTIFICATION_CHANEL_ID = "com.actosoft.noticias.test";
@@ -60,12 +67,22 @@ public class MyFirebaseInstanceService extends FirebaseMessagingService {
         }
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,NOTIFICATION_CHANEL_ID);
+        Intent notifyIntent = new Intent(this, SatisfactoryPaymentActivity.class);
+        notifyIntent.putExtra("minutes", minutes);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent notifyPendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                notifyIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
         notificationBuilder.setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL).setWhen(System.currentTimeMillis())
                 .setSmallIcon(R.drawable.ic_notification)
+                .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.ic_notification))
                 .setContentTitle(title)
                 .setContentText(body)
-                .setContentInfo("Info");
+                .setContentIntent(notifyPendingIntent);
 
         notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
 
@@ -80,7 +97,7 @@ public class MyFirebaseInstanceService extends FirebaseMessagingService {
             NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANEL_ID, "NOTIFICATION",
                     NotificationManager.IMPORTANCE_DEFAULT);
 
-            notificationChannel.setDescription("EDMT Channel");
+            notificationChannel.setDescription("Default Channel");
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.BLUE);
             notificationChannel.setVibrationPattern(new long[] {0, 1000, 500, 1000});
@@ -89,12 +106,21 @@ public class MyFirebaseInstanceService extends FirebaseMessagingService {
         }
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,NOTIFICATION_CHANEL_ID);
+        Intent notifyIntent = new Intent(this, SatisfactoryPaymentActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent notifyPendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                notifyIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
         notificationBuilder.setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL).setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.drawable.ic_notification)
+                .setSmallIcon(R.drawable.logobunti)
+                .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.logobunti))
                 .setContentTitle(title)
                 .setContentText(body)
-                .setContentInfo("Info");
+                .setContentIntent(notifyPendingIntent);
 
         notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
 
